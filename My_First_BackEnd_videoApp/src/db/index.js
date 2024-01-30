ef87e32bc080a5795e5afc9e0c266fc6a1a32b0f
Rizwan302 <rizwanahmedg2020@gmail.com>
2024-01-30 02:15:37 -0800

    env setting

--- My_First_BackEnd_videoApp/src/db/index.js
+++ My_First_BackEnd_videoApp/src/db/index.js
@@ -1,11 +1,12 @@
 import mongoose from "mongoose";
-
-
+// import DB_NAME  from "../constants.js";
+// console.log(DB_NAME) 
+// "mongodb+srv://rizwan:rizwan@cluster0.kvm3nbi.mongodb.net/youtubeApp"
 
 const connectDB = async () => {
     try {
-        const connection = await mongoose.connect("mongodb+srv://rizwan:rizwan@cluster0.kvm3nbi.mongodb.net/youtubeApp");
-        console.log(`Connect to Data Base ${connection}`)
+        const connection = await mongoose.connect(`${process.env.MONGODB_URL}/${process.env.DB_NAME}`);
+        // console.log('Connect to Data Base ', {connection})
     } catch (error) {
         console.log("ErroR Connect in MongoDB: " ,error);
         process.exit(1)
