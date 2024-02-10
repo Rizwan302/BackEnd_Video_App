c45a94f8656d6449a44f8f25e2cbc259391197df
Rizwan302 <rizwanahmedg2020@gmail.com>
2024-02-10 02:23:27 -0800

    Add styling Card

--- My_First_BackEnd_videoApp/src/controllers/user.controllers.js
+++ My_First_BackEnd_videoApp/src/controllers/user.controllers.js
@@ -240,3 +240,74 @@ export const updateCoverImage = asyncHandler(async (req, res)=>{
     return res.status(200)
     .json(new ApiResponse(200, {}, "Update Cover Image successfully"))
 })
+
+export const getUserChannelProfile = asyncHandler(async (req, res)=>{
+    const {username} = req.params;
+
+    if (!username?.trim()) {
+        throw new ApiError(400, "username is missing")
+    }
+
+    const channel = await User.aggregate([
+        {
+            $match:{
+                username: username?.toLowerCase()
+            }
+        },
+        {
+            $lookup: {
+                from: "subscriptions",
+                localField: "_id",
+                foreignField: "channel",
+                as: "subscription"
+              }
+        },
+        {
+            $lookup: {
+                from: "subscriptions",
+                localField: "_id",
+                foreignField: "subscriber",
+                as: "subscribedTo"
+              }
+        },
+
+        {
+            $addFields: {
+              subscribersCount: {
+                $size : "$subscribers"
+              }, 
+              channelSubscribersCount: {
+                $size : "$subscribedTo"
+              },
+              isSubscribe:{
+                $cond:{
+                    if:{
+                        $in:[req.user?._id, "$subscribers.subscriber"]
+                    },
+                    then: true,
+                    else: false
+                }
+              } 
+            }
+        },
+
+        {
+            $project: {
+              fullName: 1,
+              username: 1,
+              subscribersCount: 1,
+              channelSubscribersCount: 1,
+              isSubscribe: 1,
+              avatar: 1,
+              coverImage: 1,
+              email: 1,
+
+            }
+          }
+          
+    ])
+    console.log(channel)
+    if (!channel?.length) {
+        throw new ApiError(404, "Channel not exists")
+    }
+})
